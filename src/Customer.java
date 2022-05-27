2fc395ebdc739580cf79db4f5164f29135fbd0d7
Amirtharaj Balamurugan <amirtharaj.b@prodapt.com>
2022-05-27 15:22:46 +0530

    demo proj

---
+++ src/Customer.java
@@ -0,0 +1,27 @@
+
+
+public class Customer {
+	private int cutomerNumber;
+	private String customerName;
+	private int salesRepId;
+	private double creditLimit;
+
+	public Customer() {
+		
+	}
+
+	public Customer(int cutomerNumber, String customerName, int salesRepId, double creditLimit) {
+		super();
+		this.cutomerNumber = cutomerNumber;
+		this.customerName = customerName;
+		this.salesRepId = salesRepId;
+		this.creditLimit = creditLimit;
+	}
+	public void display() {
+		
+	}
+	public static Customer createCustomer(String line) {
+		
+		
+	}
+}
